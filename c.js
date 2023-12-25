4ee4f86d79c9c5bdbdc88208232187466fa0b649
Moon-o528 <148638016+Moon-o528@users.noreply.github.com>
2023-12-25 14:23:42 -0300

    Add files via upload
    Guia de viagens  referente ao trabalho final do curso de programação

---
+++ c.js
@@ -0,0 +1,44 @@
+const china = {
+    valor: 4.338,
+    distancia: 2.856,
+    hora: 33
+}
+const disney = {
+    valor: 23.599,
+    distancia: 3.299,
+    hora: 9,
+}
+const italia = {
+    valor: 3.234,
+    distancia: 9069,
+    hora: 12,
+}
+const dubai = {
+    valor: 6.516,
+    distancia: 12.346,
+    hora: 15,
+}
+const inglaterra = {
+    valor: 6.516,
+    distancia: 5784,
+    hora: 16,
+}
+const portugual = {
+    valor:  5.875,
+    distancia: 7293,
+    hora: 13,
+}
+const japao = {
+    valor: 6.923,
+    distancia: 18.460,
+    hora: 29,
+}
+
+var x = prompt("Digite sua idade")
+document.write(`Ola ${idade}, seja bem vinda`)
+if(x > 16){
+    alert("liberado")
+}while(x < 16)
+{
+    alert("Voce e muito novo para fazer compras sozinho")
+}
